f3be550bdd96cd1c1fc479bceda0b61010f3f118
loydp <54460504+loydp@users.noreply.github.com>
2020-05-16 18:41:41 +0000

    Removed stubs from ServerGateway

--- ServerGateway.cpp
+++ ServerGateway.cpp
@@ -14,105 +14,6 @@
 #define PORT 8080
 using namespace std;
 
-
-// Separated into StringParser & KeyValue, kept code commented in case of failure 
-/*
-// key/value storage structure
-class KeyValue
-{
-private:
-	char m_szKey[128];
-	char m_szValue[2048];
-public:
-	KeyValue() {};
-	void setKeyValue(char *pszBuff) {	
-		char *pch1;
-
-		// find out where the "=" sign is, and take everything to the left of the equal for the key
-		// go one beyond the = sign, and take everything else
-		pch1 = strchr(pszBuff, '=');
-		// assert(pch1);
-		int keyLen = (int)(pch1 - pszBuff);
-		strncpy(m_szKey, pszBuff, keyLen);
-		m_szKey[keyLen] = 0;
-		strcpy(m_szValue, pszBuff + keyLen + 1);
-	}
-
-	char *getKey() {
-		return m_szKey;
-	}
-
-	char *getValue() {
-		return m_szValue;
-	}
-
-};
-
-
-// This class will take a string that is passed to it in this format:
-// input to constructor:
-// <variable1>=<value1>;<variable2>=<value2>;
-// You will then call the method  getNextKeyValue until getNextKeyValue returns NULL.
-// getNextKeyValue will return a KeyValue object. Inside of that KeyValue object will contain the variable and the value
-// You will then call getKey or getValue to get the contents of those fields.
-// The example in main() will show how to call this function.
-// By extracting the contents you then can determine the rpc you need to switch to, along with variables you will need
-// You can also use this class in your client program, since you will need to determine the contents that you receive from server
-
-class stringParser
-{
-private:
-
-	char rawString[32768];                         // The original string is copied here
-	int m_currentPosition;
-	KeyValue *m_pKeyValue;
-	char *m_pch;                                   // This is another copy of it
-public:
-
-	stringParser(char *szUnformattedString)
-	{
-		// assert(strlen(szUnformattedString));     // debugging code, makes sure that there's a string maybe?
-		strcpy(rawString, szUnformattedString);     // copies the code, to prevent it from editing the original
-		m_pch = rawString;                          // copies again, into a character array
-	}
-
-	stringParser()
-	{}
-
-	~stringParser()
-	{
-		if (m_pKeyValue)
-			delete (m_pKeyValue);
-	}
-
-    void newRPC(char *szUnformattedString) {
-        // assert(strlen(szUnformattedString));         // debugging code, makes sure that there's a string maybe?
-		strcpy(rawString, szUnformattedString);     // copies the code, to prevent it from editing the original
-		m_pch = rawString;
-    }
-
-	void getNextKeyValue(KeyValue & keyVal)
-	{
-		// It will attempt to parse out part of the string all the way up to the ";", it will then create a new KeyValue object  with that partial string
-		// If it can;t it will return null;
-		char *pch1;                               // pointer character 1
-		char szTemp[32768];                       
-
-		pch1 = strchr(m_pch, ';');                // returns a pointer to the first instance of ;
-		// assert(pch1 != NULL);                     // debugging: assert that pch1 doesn't point to null
-		int subStringSize = (int)(pch1 - m_pch);  // creates a new string starting from pch1
-		strncpy(szTemp, m_pch, subStringSize);    // copies the string from m_pch to szTemp, subStringSize characters
-		szTemp[subStringSize] = 0;                // the final character is now 0; ???
-		m_pch = pch1 + 1;
-		if (m_pKeyValue)
-			delete (m_pKeyValue);
-		keyVal.setKeyValue(szTemp);               // sends szTemp to keyValue
-	}
-
-};
-
-*/
-
 // Connects the client socket to the server socket
 // return 0 if password/username passed.
 // return -2 if incorrect password
@@ -151,7 +52,7 @@ int main(int argc, char const *argv[])
     char buffer[1024] = {0};
     char DISCONNECT_RPC[1024] = "disconnected"; 
     // Added for string parser separation 
-    StringParser stringParser; 
+    StringParser stringParser;
     StringParser *parser = new StringParser; 
     // Old code before separation 
     //stringParser *parser = new stringParser();   // previous: (char *)testMSG
@@ -261,23 +162,7 @@ int main(int argc, char const *argv[])
 
         MainMenu mainMenu;
         mainMenu.loop(new_socket);
-        /*
-        This is the stub for the main RPC switch statement
 
-        while (!disconnectFlag) {
-            read(new_socket, buffer, 1024);
-
-            if (*buffer == *DISCONNECT_RPC) {
-                // IF disconnection is successful
-                if (disconnect(new_socket, buffer) == 0) 
-                    disconnectFlag = true;
-            } else {
-                // TODO: ELSE the buffered message is sent back to client (placeholder)
-                send(new_socket , buffer , strlen(buffer) , 0 ); 
-            }
-            memset(buffer, 0, sizeof(buffer));
-        }
-        */
         cout << "at the end of ServerListenLoop" << endl;
     }
 
